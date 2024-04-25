7ef9e089d34db4576fdd4e4146258015954b0dfd
Marcos Pagliaruzza <ml.paglia96@gmail.com>
2024-04-25 13:02:54 +0200

    V 1.0

---
+++ S04T01N01PagliaruzzaMarcos/src/main/java/cat/itacademy/barcelonactiva/pagliaruzza/marcos/s04/t01/n01/S04T01N01PagliaruzzaMarcos/S04T01N01PagliaruzzaMarcosApplication.java
@@ -0,0 +1,13 @@
+package cat.itacademy.barcelonactiva.pagliaruzza.marcos.s04.t01.n01.S04T01N01PagliaruzzaMarcos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S04T01N01PagliaruzzaMarcosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S04T01N01PagliaruzzaMarcosApplication.class, args);
+	}
+
+}
---
+++ S04T01N01PagliaruzzaMarcos/src/test/java/cat/itacademy/barcelonactiva/pagliaruzza/marcos/s04/t01/n01/S04T01N01PagliaruzzaMarcos/S04T01N01PagliaruzzaMarcosApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.itacademy.barcelonactiva.pagliaruzza.marcos.s04.t01.n01.S04T01N01PagliaruzzaMarcos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S04T01N01PagliaruzzaMarcosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
